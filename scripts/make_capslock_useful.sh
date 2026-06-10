e4dd081620840529eb3a319b4e3569e7eb43d254
A7reus <an1ndya@proton.me>
2026-06-10 12:20:04 +0600

    [scripts] replace doas with sudo for capslock setup

--- scripts/make_capslock_useful.sh
+++ scripts/make_capslock_useful.sh
@@ -3,12 +3,12 @@
 set -e
 
 if ! pacman -Q interception-tools interception-caps2esc &> /dev/null; then
-  if command -v pacman &> /dev/null && command -v doas &> /dev/null; then
+  if command -v pacman &> /dev/null && command -v sudo &> /dev/null; then
     printf "Installing 'interception-tools' and 'interception-caps2esc'...\n\n"
   
-    doas pacman -S interception-tools interception-caps2esc
+    sudo pacman -S interception-tools interception-caps2esc
   else
-    printf "doas and/or pacman not found, exitting..."
+    printf "sudo and/or pacman not found, exitting..."
     exit 1
   fi
   else
@@ -17,8 +17,8 @@ fi
 
 printf "Creating system-wide configuration for caps2esc...\n\n"
 
-doas mkdir -p /etc/interception/udevmon.d
-doas tee /etc/interception/udevmon.d/caps2esc.yaml > /dev/null <<EOL
+sudo mkdir -p /etc/interception/udevmon.d
+sudo tee /etc/interception/udevmon.d/caps2esc.yaml > /dev/null <<EOL
 - JOB: "intercept -g \$DEVNODE | caps2esc | uinput -d \$DEVNODE"
   DEVICE:
     EVENTS:
@@ -27,8 +27,8 @@ EOL
 
 printf "Setting up systemd override...\n\n"
 
-doas mkdir -p /etc/systemd/system/udevmon.service.d
-doas tee /etc/systemd/system/udevmon.service.d/override.conf > /dev/null <<EOL
+sudo mkdir -p /etc/systemd/system/udevmon.service.d
+sudo tee /etc/systemd/system/udevmon.service.d/override.conf > /dev/null <<EOL
 [Service]
 ExecStart=
 ExecStart=/usr/bin/udevmon -c /etc/interception/udevmon.d/caps2esc.yaml
@@ -36,9 +36,9 @@ EOL
 
 printf "Reloading systemd and starting daemon...\n\n"
 
-doas systemctl daemon-reload
-doas systemctl restart udevmon
-doas systemctl enable udevmon
+sudo systemctl daemon-reload
+sudo systemctl restart udevmon
+sudo systemctl enable udevmon
 
 printf "All done, exitting...\n\n"
 exit 0
